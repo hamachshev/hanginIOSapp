5d0e8c72ba4813c7bda76472552ab7127d0a4dc3
Aharon Seidman <aharondov1@gmail.com>
2024-09-12 23:55:50 -0400

    Initial Commit

---
+++ hangin/hanginApp.swift
@@ -0,0 +1,17 @@
+//
+//  hanginApp.swift
+//  hangin
+//
+//  Created by Aharon Seidman on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct hanginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
